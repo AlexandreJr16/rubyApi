7d4fd14f69dd42428c761d202ca330407a1900e3
Alexandre Junior <gamerapsj@gmail.com>
2023-10-18 14:56:37 -0300

    Fazendo autentificação

--- app/controllers/v1/sessions_controller.rb
+++ app/controllers/v1/sessions_controller.rb
@@ -3,6 +3,13 @@ class V1::SessionsController < ApplicationController
 
   #Create is like sign-in
   def create
+    user = User.where(email: params[:email]).first
+
+    if user&.valid_password?(params[:password])
+      render json: user.as_json(only: [:email, :id]), status: :created
+    else
+      head(:unauthorized)
+    end
 
   end
 
